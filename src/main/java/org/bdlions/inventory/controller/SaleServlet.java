58e7ab00d474c2f3bef562582472a1fa4465459a
nazmulhasan <bdlions@gmail.com>
2017-11-20 14:37:31 +0600

    Sale Order Report partial progress

--- src/main/java/org/bdlions/inventory/controller/SaleServlet.java
+++ src/main/java/org/bdlions/inventory/controller/SaleServlet.java
@@ -16,18 +16,26 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.bdlions.inventory.dto.DTOCustomer;
 import org.bdlions.inventory.dto.DTOProduct;
 import org.bdlions.inventory.dto.DTOPurchaseOrder;
+import org.bdlions.inventory.dto.DTOSaleOrder;
 import org.bdlions.inventory.dto.DTOSupplier;
+import org.bdlions.inventory.entity.EntityCustomer;
 import org.bdlions.inventory.entity.EntityPOShowRoomProduct;
 import org.bdlions.inventory.entity.EntityProduct;
 import org.bdlions.inventory.entity.EntityPurchaseOrder;
+import org.bdlions.inventory.entity.EntitySaleOrder;
+import org.bdlions.inventory.entity.EntitySaleOrderProduct;
 import org.bdlions.inventory.entity.EntityShowRoomStock;
 import org.bdlions.inventory.entity.EntitySupplier;
 import org.bdlions.inventory.entity.EntityUser;
+import org.bdlions.inventory.entity.manager.EntityManagerCustomer;
 import org.bdlions.inventory.entity.manager.EntityManagerPOShowRoomProduct;
 import org.bdlions.inventory.entity.manager.EntityManagerProduct;
 import org.bdlions.inventory.entity.manager.EntityManagerPurchaseOrder;
+import org.bdlions.inventory.entity.manager.EntityManagerSaleOrder;
+import org.bdlions.inventory.entity.manager.EntityManagerSaleOrderProduct;
 import org.bdlions.inventory.entity.manager.EntityManagerShowRoomStock;
 import org.bdlions.inventory.entity.manager.EntityManagerSupplier;
 import org.bdlions.inventory.entity.manager.EntityManagerUser;
@@ -38,68 +46,68 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
- * @author alamgir
+ * @author nazmul hasan
  */
 @CrossOrigin
 @RestController
-public class PurchaseServlet {
+public class SaleServlet {
 
-    @RequestMapping("/purchasereport")
+    @RequestMapping("/salereport")
     public void getReport(HttpServletResponse response) {
 
         response.setContentType("application/pdf");
 
-        String sourceFileName = getClass().getClassLoader().getResource("reports/purchase.jasper").getFile();
+        String sourceFileName = getClass().getClassLoader().getResource("reports/sale.jasper").getFile();
         
         EntityManagerUser entityManagerUser = new EntityManagerUser();
         
-        DTOSupplier dtoSupplier = new DTOSupplier();
-        dtoSupplier.setEntitySupplier(new EntitySupplier());
-        dtoSupplier.setEntityUser(new EntityUser());
+        DTOSaleOrder dtoSaleOrder = new DTOSaleOrder();
+        dtoSaleOrder.setEntitySaleOrder(new EntitySaleOrder());
+        dtoSaleOrder.getEntitySaleOrder().setOrderNo("order1");
         
-        DTOPurchaseOrder dtoPurchaseOrder = new DTOPurchaseOrder();
-        dtoPurchaseOrder.setEntityPurchaseOrder(new EntityPurchaseOrder());
-        dtoPurchaseOrder.getEntityPurchaseOrder().setOrderNo("order1");
-        EntityManagerPurchaseOrder entityManagerPurchaseOrder = new EntityManagerPurchaseOrder();
-        EntityPurchaseOrder entityPurchaseOrder = entityManagerPurchaseOrder.getPurchaseOrderByOrderNo(dtoPurchaseOrder.getEntityPurchaseOrder().getOrderNo());
-        if(entityPurchaseOrder != null)
+        DTOCustomer dtoCustomer = new DTOCustomer();
+        dtoCustomer.setEntityCustomer(new EntityCustomer());
+        dtoCustomer.setEntityUser(new EntityUser());
+        
+        EntityManagerSaleOrder entityManagerSaleOrder = new EntityManagerSaleOrder();
+        EntitySaleOrder entitySaleOrder = entityManagerSaleOrder.getSaleOrderByOrderNo(dtoSaleOrder.getEntitySaleOrder().getOrderNo());
+        if(entitySaleOrder != null)
         {
-            dtoPurchaseOrder.setEntityPurchaseOrder(entityPurchaseOrder);
-            EntityManagerPOShowRoomProduct entityManagerPOShowRoomProduct = new EntityManagerPOShowRoomProduct();
-            List<EntityPOShowRoomProduct> entityPOShowRoomProducts = entityManagerPOShowRoomProduct.getPOShowRoomProductsByOrderNo(dtoPurchaseOrder.getEntityPurchaseOrder().getOrderNo());
-            if(entityPOShowRoomProducts != null && !entityPOShowRoomProducts.isEmpty())
+            dtoSaleOrder.setEntitySaleOrder(entitySaleOrder);
+            EntityManagerSaleOrderProduct entityManagerSaleOrderProduct = new EntityManagerSaleOrderProduct();
+            List<EntitySaleOrderProduct> entitySaleOrderProducts = entityManagerSaleOrderProduct.getSaleOrderProductsByOrderNo(dtoSaleOrder.getEntitySaleOrder().getOrderNo());
+            if(entitySaleOrderProducts != null && !entitySaleOrderProducts.isEmpty())
             {
                 EntityManagerShowRoomStock entityManagerShowRoomStock = new EntityManagerShowRoomStock();
                 EntityManagerProduct entityManagerProduct = new EntityManagerProduct();
-                for(int counter = 0; counter < entityPOShowRoomProducts.size(); counter++)
+                for(int counter = 0; counter < entitySaleOrderProducts.size(); counter++)
                 {
-                    EntityPOShowRoomProduct entityPOShowRoomProduct = entityPOShowRoomProducts.get(counter);
-                    EntityShowRoomStock stockProduct = entityManagerShowRoomStock.getShowRoomProductByPurchaseOrderNoAndTransactionCategoryId(entityPOShowRoomProduct.getProductId(), dtoPurchaseOrder.getEntityPurchaseOrder().getOrderNo(), Constants.SS_TRANSACTION_CATEGORY_ID_PURCASE_IN);
+                    EntitySaleOrderProduct entitySaleOrderProduct = entitySaleOrderProducts.get(counter);
+                    EntityShowRoomStock stockProduct = entityManagerShowRoomStock.getShowRoomProductBySaleOrderNoAndTransactionCategoryId(entitySaleOrderProduct.getProductId(), dtoSaleOrder.getEntitySaleOrder().getOrderNo(), Constants.SS_TRANSACTION_CATEGORY_ID_SALE_OUT);
                     EntityProduct entityProduct = entityManagerProduct.getProductByProductId(stockProduct.getProductId());
 
                     DTOProduct dtoProduct = new DTOProduct();
-                    dtoProduct.setQuantity(stockProduct.getStockIn());
+                    dtoProduct.setQuantity(stockProduct.getStockOut());
                     dtoProduct.setEntityProduct(entityProduct);
-                    dtoProduct.getEntityProduct().setUnitPrice(entityPOShowRoomProduct.getUnitPrice());
-                    dtoPurchaseOrder.getProducts().add(dtoProduct);
+                    dtoProduct.getEntityProduct().setUnitPrice(entitySaleOrderProduct.getUnitPrice());
+                    dtoSaleOrder.getProducts().add(dtoProduct);
                 }
-            }
-            EntityManagerSupplier entityManagerSupplier = new EntityManagerSupplier();
-            EntitySupplier entitySupplier = entityManagerSupplier.getSupplierByUserId(dtoPurchaseOrder.getEntityPurchaseOrder().getSupplierUserId());
-            dtoSupplier.setEntitySupplier(entitySupplier);
-            dtoSupplier.setEntityUser(entityManagerUser.getUserByUserId(dtoSupplier.getEntitySupplier().getUserId()));
-        } 
-        
+            }            
+            EntityManagerCustomer entityManagerCustomer = new EntityManagerCustomer();
+            EntityCustomer entityCustomer = entityManagerCustomer.getCustomerByUserId(dtoSaleOrder.getEntitySaleOrder().getCustomerUserId());
+            dtoCustomer.setEntityCustomer(entityCustomer);
+            
+            dtoCustomer.setEntityUser(entityManagerUser.getUserByUserId(dtoCustomer.getEntityCustomer().getUserId()));
+        }
         
         Map parameters = new HashMap();
         parameters.put("Date", "2017-11-20");
-        parameters.put("OrderNo", dtoPurchaseOrder.getEntityPurchaseOrder().getOrderNo());
-        parameters.put("SupplierName", dtoSupplier.getEntityUser().getFirstName()+" "+dtoSupplier.getEntityUser().getLastName());
+        parameters.put("OrderNo", dtoSaleOrder.getEntitySaleOrder().getOrderNo());
+        parameters.put("CustomerName", dtoCustomer.getEntityUser().getFirstName()+" "+dtoCustomer.getEntityUser().getLastName());
         parameters.put("Address", "Dhaka, Bangladesh");
-        parameters.put("Email", dtoSupplier.getEntityUser().getEmail());
-        parameters.put("Phone", dtoSupplier.getEntityUser().getCell());
+        parameters.put("Email", dtoCustomer.getEntityUser().getEmail());
+        parameters.put("Phone", dtoCustomer.getEntityUser().getCell());
         
-        //EntityManagerUser entityManagerUser = new EntityManagerUser();
         List<EntityUser> dataList = entityManagerUser.getUsers(1, 10);
 
         JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(dataList);
