5a9a99fabf84474d1be02f4be737a005ebd5ed6a
nazmulhasan <bdlions@gmail.com>
2018-01-31 23:46:59 +0600

    Run the project from Jar

---
+++ src/main/java/org/bdlions/inventory/JarRunner.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bdlions.inventory;
+
+import org.bdlions.inventory.db.DatabaseLoader;
+import org.bdlions.inventory.db.HibernateUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class JarRunner{
+
+
+    public static void main(String[] args){
+        SpringApplication.run(JarRunner.class, args);
+        //OrganizationLoaderManager.getInstance().start();
+        
+        DatabaseLoader.getInstance().getSession();
+        HibernateUtil.getInstance().getSession();
+        
+        System.out.println("Server started");
+    }
+
+}
