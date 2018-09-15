c9ac00ddcd95ca5f50ec4fb959647031bc1b23f4
nazmulhasan <bdlions@gmail.com>
2018-09-15 19:10:44 +0600

    Licesne Logic is added to run it from client machine

--- src/main/java/org/bdlions/inventory/Main.java
+++ src/main/java/org/bdlions/inventory/Main.java
@@ -4,20 +4,39 @@ import org.bdlions.inventory.db.DatabaseLoader;
 import org.bdlions.inventory.db.HibernateUtil;
 import org.bdlions.inventory.entity.EntityOrganization;
 import org.bdlions.inventory.entity.manager.EntityManagerOrganization;
-import org.bdlions.session.ISessionManager;
+import org.bdlions.inventory.util.LicenseKeyProvider;
+import org.bdlions.license.KeyManager;
+import org.bdlions.license.util.LicenseUtility;
 import org.bdlions.transport.channel.provider.ChannelProviderImpl;
-import org.bdlions.inventory.util.ClientRequestHandler;
-import org.bdlions.util.handler.request.IClientRequestHandler;
 import org.hibernate.Session;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Main extends SpringBootServletInitializer {
 
     private static ChannelProviderImpl channelProviderImpl = null;
+    
+    @Bean
+    WebMvcConfigurer configurer () {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers (ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/html/**").
+                          addResourceLocations("classpath:/");
+            }
+        };
+    }
     
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -25,6 +44,30 @@ public class Main extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) throws Exception {
+        //This is licnese checking logic. Use it while installing into customer machine.
+        /*KeyManager km = new KeyManager();
+        int seed = LicenseUtility.getInstance().getSeed();
+        String key = km.generateKey();
+
+        String serialNumber = LicenseKeyProvider.get("LICENSE_NUMBER");
+        if(serialNumber != null && !serialNumber.isEmpty())
+        {
+            if(serialNumber.equals(seed+key))
+            {
+                System.out.println("Valid License.");
+            }
+            else
+            {   
+                System.out.println("Invalid License.");
+                return;
+            }
+        }
+        else
+        {   
+            System.out.println("Invalid License.");
+            return;
+        }*/
+        
         SpringApplication.run(Main.class, args);
         
 //        IClientRequestHandler requestHandler = ClientRequestHandler.getInstance();
